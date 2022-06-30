d69ab1e5af2b4e0d56cff6620eceb6b5d6199596
Darksied95 <aliyuefe9501@gmail.com>
2022-06-30 11:35:31 +0100

    first commits

---
+++ src/Question.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = () => {
+  return <div>
+    <h2></h2>
+  </div>;
+};
+
+export default Question;
